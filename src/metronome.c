cdc24db015b9357fecfa8dd2f4dbc31671194b00
armandoarenas <armandoarenas@172.16.241.1>
2019-04-15 20:17:02 -0400

    Done deal

--- src/metronome.c
+++ src/metronome.c
@@ -13,21 +13,27 @@
 #define MY_PULSE_CODE _PULSE_CODE_MINAVAIL
 #define PAUSE_PULSE_CODE (MY_PULSE_CODE+1)
 #define QUIT_PULSE_CODE	(MY_PULSE_CODE+2)
+#define INFO_PULSE_CODE (MY_PULSE_CODE+3)
+
+void *metronomeThread(void* argc);
 
 char data[255];
 
 int server_coid;
+
+pthread_t pthread;
 
 typedef union {
 	struct _pulse pulse;
 	char msg[255];
 } my_message_t;
 
-struct signature {
+struct threadAttribute {
 	int bpm;
 	int tst;
 	int tsb;
-}typedef signature_t;
+	name_attach_t* attach;
+}typedef threadAttribute_t;
 
 struct DataTableRow {
 	int tst;
@@ -40,6 +46,9 @@ DataTableRow t[] = { { 2, 4, 4, "|1&2&" }, { 3, 4, 6, "|1&2&3&" }, { 4, 4, 8,
 		"|1&2&3&4&" }, { 5, 4, 10, "|1&2&3&4-5-" }, { 3, 8, 6, "|1-2-3-" }, { 6,
 		8, 6, "|1&a2&a" }, { 9, 8, 9, "|1&a2&a3&a" }, { 12, 8, 12,
 		"|1&a2&a3&a4&a" } };
+
+threadAttribute_t threadAttr;
+int RUNNING = 1;
 
 int io_read(resmgr_context_t *ctp, io_read_t *msg, RESMGR_OCB_T *ocb) {
 
@@ -79,7 +88,7 @@ int io_write(resmgr_context_t *ctp, io_write_t *msg, RESMGR_OCB_T *ocb) {
 	// verify if the msg was sent
 	if (msg->i.nbytes == ctp->info.msglen - (ctp->offset + sizeof(*msg))) {
 		/* have all the data */
-		signature_t sig;
+		//signature_t sig;
 		char *buf;
 		char *alert_msg;
 		int i, pauseAmount;
@@ -93,17 +102,20 @@ int io_write(resmgr_context_t *ctp, io_write_t *msg, RESMGR_OCB_T *ocb) {
 			pauseAmount = atoi(strsep(&buf, " "));
 			if (pauseAmount >= 1 && pauseAmount <= 9) {
 				//FIXME :: replace getprio() with SchedGet()
-				MsgSendPulse(server_coid, SchedGet(0, 0, NULL), MY_PULSE_CODE, pauseAmount);
+				MsgSendPulse(server_coid, SchedGet(0, 0, NULL), PAUSE_PULSE_CODE, pauseAmount);
 			} else {
-				printf("Integer is not between 1 and 9.\n");
+				printf("\nInteger is not between 1 and 9.\n");
 			}
 		} else if (strstr(alert_msg, "info") != NULL) {
-			//metronome [<bpm> beats/min, time signature <ts-top>/<ts-bottom>
-			////
-			sprintf(data, "%d beats/min, time signature %d/%d", sig.bpm, sig.tst, sig.tsb);
+			MsgSendPulse(server_coid, SchedGet(0, 0, NULL), INFO_PULSE_CODE, 0);
 
 		} else if (strstr(alert_msg, "quit") != NULL) {
 			MsgSendPulse(server_coid, SchedGet(0, 0, NULL), QUIT_PULSE_CODE, 0);
+
+			pthread_join(pthread, NULL);
+			exit(EXIT_SUCCESS);
+		} else {
+			sprintf(data, "Error Incorrect Input\n");
 		}
 
 		nb = msg->i.nbytes;
@@ -126,86 +138,103 @@ int io_open(resmgr_context_t *ctp, io_open_t *msg, RESMGR_HANDLE_T *handle,
 	return (iofunc_open_default(ctp, msg, handle, extra));
 }
 
-void *metronomeThread(void* argc) {			// client
+void *metronomeThread(void* argc) {
 	struct sigevent event;
 	struct itimerspec itime;
 	timer_t timer_id;
 	int chid;
 	int rcvid;
+	int coid;
+	int selected;
 	my_message_t msg;
-	signature_t* signature;
+	threadAttribute_t* threadAttr;
 
-	signature = argc;
-	chid = ChannelCreate(0);
+	threadAttr = argc;
+	chid = threadAttr->attach->chid;
 
 	event.sigev_notify = SIGEV_PULSE;
-	event.sigev_coid = ConnectAttach(ND_LOCAL_NODE, 0, chid, _NTO_SIDE_CHANNEL, 0);
-	//event.sigev_priority = SchedGet(0, 0, NULL);
+	coid = ConnectAttach(ND_LOCAL_NODE, 0, chid, _NTO_SIDE_CHANNEL, 0);
+	event.sigev_coid = coid;
+
 	event.sigev_code = MY_PULSE_CODE;
 	timer_create(CLOCK_REALTIME, &event, &timer_id);
 
-	//#metronome 120 2 4
-	/*60 sec / 120 beats = 0.5 sec / beat
-	 * 0.5 sec/beat * 2 beat/measure = 1 second per measure
-	 * (1 sec) / (4 intervals) = 0.25 sec /interval
-	 * */
-
-	double a = 60 / (double) signature->bpm; // Seconds / Beat
-	double b = a * (double) signature->tst; // Seconds per measure
-	double c = b / (double) signature->tsb; // Intervals seconds
-
-	int _c = (int)c;
-	printf("Beats/second(a): %f\n", a);
-	printf("Seconds per measure(b): %f\n", b);
-	printf("Interval Seconds(c): %f\n", c);
-
-	printf("Integer: %d\n", _c);
-
-	printf("Value: %f\n", c * 1000000000);
-	itime.it_value.tv_nsec = c * 1000000000;
-	itime.it_interval.tv_nsec = c * 1000000000;
-
-//	itime.it_value.tv_sec = 1;
-//	/* 500 million nsecs = .5 secs */
-//	itime.it_value.tv_nsec = 500000000;
-//	itime.it_interval.tv_sec = 1;
-//	/* 500 million nsecs = .5 secs */
-//	itime.it_interval.tv_nsec = 500000000;
-
-	timer_settime(timer_id, 0, &itime, NULL);
-
-	/*
-	 * As of the timer_settime(), we will receive our pulse
-	 * in 1.5 seconds (the itime.it_value) and every 1.5
-	 * seconds thereafter (the itime.it_interval)
-	 */
-	int selected;
-
 	for (int i = 0; i < 8; i++) {
-		if (t[i].tsb == signature->tsb && t[i].tst == signature->tst) {
+		if (t[i].tsb == threadAttr->tsb && t[i].tst == threadAttr->tst) {
 			selected = i;
 			break;
 		}
 	}
 
+	double a = 60 / (double) threadAttr->bpm; // Seconds / Beat
+	double b = a * (double) threadAttr->tst; // Seconds per measure
+	double c = b / t[selected].intervals; // Intervals seconds
+
+	int _c = (int) c;
+	printf("Beats/second(a): %f\n", a);
+	printf("Seconds per measure(b): %f\n", b);
+	printf("Interval Seconds(c): %f\n", c);
+
+	printf("Integer of c: %d\n", _c);
+
+	printf("Value: %f\n", c * 1000000000);
+	if (_c > 0){ // Interval is more than a second.
+		itime.it_value.tv_sec = _c;
+		itime.it_interval.tv_sec = _c;
+		itime.it_value.tv_nsec = (c - _c) * 1000000000;
+		itime.it_interval.tv_nsec = (c - _c) * 1000000000;
+	} else {
+		itime.it_value.tv_nsec = c * 1000000000;
+		itime.it_interval.tv_nsec = c * 1000000000;
+	}
+
+	timer_settime(timer_id, 0, &itime, NULL);
+
 	printf("String: %s\n", t[selected].output);
+
+	setvbuf(stdout, NULL, _IONBF, 0);
 
 	for (;;) {
 		fprintf(stderr, "|");
 		for (int i = 0; i < t[selected].intervals; i++) {
+
 			rcvid = MsgReceive(chid, &msg, sizeof(msg), NULL);
+
+			if (rcvid == -1){
+				MsgError(rcvid, ENOSYS);
+				return EXIT_FAILURE;
+			}
+
 			if (rcvid == 0) { /* we got a pulse */
 				if (msg.pulse.code == MY_PULSE_CODE) {
-					fprintf(stderr, "%c", t[selected].output[i+1]);
-				} /* else other pulses ... */
-			} /* else other messages ... */
+					printf("%c", t[selected].output[i + 1]);
+				}
+				if (msg.pulse.code == QUIT_PULSE_CODE) {
+					printf("quitting \n");
+
+					timer_delete(timer_id); // Delete timer
+					ConnectDetach(coid); // Detach Connection
+					name_detach(threadAttr->attach, 0); //Detach from named channel
+					name_close(server_coid);
+
+					return EXIT_SUCCESS;
+				}
+				if (msg.pulse.code == PAUSE_PULSE_CODE) {
+					itime.it_value.tv_sec = msg.pulse.value.sival_int;
+					timer_settime(timer_id, 0, &itime, NULL);
+				}
+				if (msg.pulse.code == INFO_PULSE_CODE){
+					sprintf(data, "%d beats/min\ttime signature %d/%d\n", threadAttr->bpm, threadAttr->tst, threadAttr->tsb);
+				}
+			}
 		}
-		fprintf(stderr, "\n");
+		printf("\n");
 	}
+
 	return EXIT_SUCCESS;
 }
 
-int main(int argc, char *argv[]) { // server
+int main(int argc, char *argv[]) {
 	dispatch_t* dpp;
 	resmgr_io_funcs_t io_funcs;
 	resmgr_connect_funcs_t connect_funcs;
@@ -213,7 +242,7 @@ int main(int argc, char *argv[]) { // server
 	dispatch_context_t *ctp;
 	pthread_attr_t attr;
 	name_attach_t *attach;
-	signature_t signature;
+	threadAttribute_t threadAttr;
 	int id;
 
 	if (argc != 4) {
@@ -221,17 +250,17 @@ int main(int argc, char *argv[]) { // server
 		return EXIT_FAILURE;
 	}
 
-	int bpm = atoi(argv[1]);
-	int tst = atoi(argv[2]);
-	int tsb = atoi(argv[3]);
+	if ((attach = name_attach(NULL, "metronome", 0)) == NULL) {
+		return EXIT_FAILURE;
+	}
 
-	signature.bpm = bpm;
-	signature.tst = tst;
-	signature.tsb = tsb;
+	threadAttr.bpm = atoi(argv[1]);
+	threadAttr.tst = atoi(argv[2]);
+	threadAttr.tsb = atoi(argv[3]);
+	threadAttr.attach = attach;
 
 	dpp = dispatch_create();
-	iofunc_func_init(_RESMGR_CONNECT_NFUNCS, &connect_funcs, _RESMGR_IO_NFUNCS,
-			&io_funcs);
+	iofunc_func_init(_RESMGR_CONNECT_NFUNCS, &connect_funcs, _RESMGR_IO_NFUNCS, &io_funcs);
 	connect_funcs.open = io_open;
 	io_funcs.read = io_read;
 	io_funcs.write = io_write;
@@ -241,7 +270,7 @@ int main(int argc, char *argv[]) { // server
 	id = resmgr_attach(dpp, NULL, "/dev/local/metronome", _FTYPE_ANY, NULL, &connect_funcs, &io_funcs, &ioattr);
 
 	pthread_attr_init(&attr);
-	pthread_create(NULL, &attr, &metronomeThread, &signature);
+	pthread_create(&pthread, &attr, &metronomeThread, &threadAttr);
 	pthread_attr_destroy(&attr);
 
 	ctp = dispatch_context_alloc(dpp);
@@ -249,6 +278,6 @@ int main(int argc, char *argv[]) { // server
 		ctp = dispatch_block(ctp);
 		dispatch_handler(ctp);
 	}
+
 	return EXIT_SUCCESS;
 }
-
